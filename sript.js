55720949e9c50e61633afb2b8647a96c54b2bcce
Edina05 <ets.deliedina@gmail.com>
2023-02-14 12:09:54 +0100

    van hf

--- sript.js
+++ sript.js
@@ -6,8 +6,25 @@ console.dir(document);
 //document.getElementById("main_title").textContent = "harmadik cim";
 //let cim = document.getElementById("main_title");
 //cim.textContent = "negyedik cim";
-for (let i = 0; i < items.lenght; i++) {
+/*for (let i = 0; i < items.lenght; i++) {
   if (i % 2 == 0) {
     item[i].style.backgraundColor = "lightgrey";
   }
-}
+}*/
+/*console.log(document.getElementsByTagName("h2"));
+let h2= document.getElementsByTagName*/
+//id #
+//class .
+//tag akkor semmi nem megy elebe
+/*console.log(document.querySelector("elso a"));
+
+let elsoLink = dokument.querySelector("#elso a);
+elsoLink.style.color = "red";*/
+
+/*console.log(document.querySelector("elso p"));
+
+let elsoParagraf = dokument.querySelector("#elso p");
+elsoParagraf.style.color = "red";*/
+console.log(document.querySelectorAll(".title"));
+let lista = document.querySelectorAll(".title");
+lista[1].style.color = "pink";
