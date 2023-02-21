3e3d519a7462253c06de98ad316247e9c6d69c20
Edina05 <ets.deliedina@gmail.com>
2023-02-21 15:55:14 +0100

    uzi

--- sript.js
+++ sript.js
@@ -16,7 +16,7 @@ let h2= document.getElementsByTagName*/
 //id #
 //class .
 //tag akkor semmi nem megy elebe
-/*console.log(document.querySelector("elso a"));
+/*console.log(document.querySelector("elso a"));// css 
 
 let elsoLink = dokument.querySelector("#elso a);
 elsoLink.style.color = "red";*/
@@ -25,6 +25,23 @@ elsoLink.style.color = "red";*/
 
 let elsoParagraf = dokument.querySelector("#elso p");
 elsoParagraf.style.color = "red";*/
-console.log(document.querySelectorAll(".title"));
+/*console.log(document.querySelectorAll(".title"));
 let lista = document.querySelectorAll(".title");
-lista[1].style.color = "pink";
+lista[1].style.color = "pink";*/
+//html oldal 5 fuggveny
+//getbyid,byclass,tagname, queryselektror,meg all
+//.perentNode kiválasszuk a szülőt objekt
+//.perentElement Hasolno objekt
+//childNode minden ami a gyerekben van (array)
+// first-. last-objekt
+// children a tányak azaz elemnetek amik benne vannak (array)
+//firstElementChild objekt
+//lastElementChod objekt
+//sibling (next-, previous-) utánna és előtte lévő dolgok pl text
+/*let paragraf = document.querySelector("#masodik p");
+console.log(paragraf.previousElementSibling);*/
+/*let paragraf = document.querySelector("#masodik p");
+console.log(paragraf.previousSibling.previousSibling);// kozte van a text*/
+console.log(document.getElementById("container"));
+let container = document.getElementById("container");
+container.children[2].style.backgroundColor = "green";
